806b0ff1e4d56cfd5d1c9b761d2aaa34c05baee6
Adam Helmi <adamhelmi@kakitangan.com>
2025-07-05 21:30:34 +0800

    Initial commit (Page Routers)

---
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <div className="lg:w-[70%] j-3/4 md:h-full flex flex-col justify-center"></div>
+      <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+      <p className="">Please enter your details to log in</p>
+    </div>
+  );
+};
+
+export default Login;
